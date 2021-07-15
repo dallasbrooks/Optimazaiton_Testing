56ae8fa81870e0811e6f7a7b4d0535841e4f8e0e
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-07-15 10:44:42 -0700

    Create Linear_Search_Recursive.c

---
+++ Resources/Search/Linear_Search_Recursive.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void printArray(int arr[], int n);
+
+int recursiveLinearSearch(int arr[], int n, int idx, int x){
+    if(idx >= n){
+        return -1;
+    }
+    return arr[idx] == x ? idx : recursiveLinearSearch(arr, n, idx+1, x);
+}
+
+int main(){
+	int arr[] = {21, 26, 13, 5, 29, 6, 12, 9, 20, 19, 14, 17, 15, 22, 18, 24, 27, 4, 16, 30, 2, 7, 25, 11, 10};
+	int arr_length = sizeof(arr)/sizeof(arr[0]);
+	int x = 18;
+	printArray(arr, arr_length);
+	printf("Looking for %d\n", x);
+	int idx = recursiveLinearSearch(arr, arr_length, 0, x);
+	if(idx != -1){
+		printf("Found %d at index %d\n", x, idx);
+	}else{
+		printf("Did not find %d\n", x);
+	}
+	
+	return 0;
+}
+
+void printArray(int arr[], int n){
+	for(int a = 0; a < n; a++){
+		printf("%d ", arr[a]);
+	}
+	printf("\n");
+}
