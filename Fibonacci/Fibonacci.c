f9db4633fe0cc7745b4336d667a76de9d0d9c9a6
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-07-14 22:53:31 -0700

    initial fib problem

---
+++ Fibonacci/Fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+
+//(1 + sqrt(5)) / 2
+#define PHI 1.6180339887498948482045
+#define Xn 50
+
+struct timeval start;	
+
+double fib_Function(int n){
+	return (pow(PHI, n) - pow(1-PHI, n)) / sqrt(5);
+}
+
+double fib_Recursion(double n){
+	return n < 2 ? n : fib_Recursion(n-1) + fib_Recursion(n-2);
+}
+
+float getTime(){
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	return (now.tv_sec - start.tv_sec) + (now.tv_usec - start.tv_usec)/1000000.0f;
+}
+
+int main(){
+	gettimeofday(&start, NULL);	
+	for(int a = 0; a < Xn; a++){
+		printf("%d\t%.0f\n", a, round(fib_Function(a)));
+	}
+	double funct = getTime();
+	gettimeofday(&start, NULL);
+	for(int a = 0; a < Xn; a++){
+		printf("%d\t%.0f\n", a, fib_Recursion(a));
+	}
+	double recurs = getTime();
+	printf("Equation is: %f\n", funct);
+	printf("Recursion is: %f\n", recurs);
+	return 0;
+}
---
+++ Fibonacci/readme.md
@@ -0,0 +1,17 @@
+# Fibonacci Nth Term
+
+For this program I tried to show a better solution to finding the Nth term of a Fibonacci sequence.
+
+In this program the golden ratio is used in the Equation version, while Recursion is used for the naive version.
+
+To compile:
+```
+gcc Fibonacci.c -lm -o Fibonacci
+
+```
+
+To run:
+```
+./Fibonacci
+
+```
