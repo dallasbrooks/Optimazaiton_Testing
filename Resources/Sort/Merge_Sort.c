e8e7d091cefad21ea3496ebc7f3e869d907c0bae
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-07-20 10:05:39 -0700

    refactor merge function

--- Resources/Sort/Merge_Sort.c
+++ Resources/Sort/Merge_Sort.c
@@ -4,12 +4,13 @@ void printArray(int arr[], int n);
 void merge(int arr[], int l, int m, int r);
 
 void mergeSort(int arr[], int l, int r){
-	if(l < r){
-		int m = l + (r - l) / 2;
-		mergeSort(arr, l, m);
-		mergeSort(arr, m + 1, r);
-		merge(arr, l, m, r);
+	if(l >= r){
+		return;
 	}
+	int m = (l + r) / 2;
+	mergeSort(arr, l, m);
+	mergeSort(arr, m+1, r);
+	merge(arr, l, m, r);
 }
 
 int main(){
@@ -29,37 +30,33 @@ void printArray(int arr[], int n){
 }
 
 void merge(int arr[], int l, int m, int r){
-	int i, j, k;
-	int n1 = m - l + 1;
-	int n2 = r - m;
-	int L[n1], R[n2];
-	for(i = 0; i < n1; i++){
-		L[i] = arr[l + i];
+	int Ln = m - l + 1;
+	int Rn = r - m;
+	int L[Ln];
+	int R[Rn];
+	for(int a = 0; a < Ln; a++){
+		L[a] = arr[l + a];
 	}
-	for(j = 0; j < n2; j++){
-		R[j] = arr[m + 1 + j];
+	for(int a = 0; a < Rn; a++){
+		R[a] = arr[m + 1 + a];
 	}
-	i = 0;
-	j = 0;
-	k = l;
-	while(i < n1 && j < n2){
-		if(L[i] <= R[j]){
-			arr[k] = L[i];
-			i++;
-		}else{
-			arr[k] = R[j];
-			j++;
+	int ll = 0;
+	int rr = 0;
+	for(int a = l; a < l+r+1; a++){
+		if(ll < Ln && rr < Rn){
+			if(L[ll] <= R[rr]){
+				arr[a] = L[ll];
+				ll++;
+			}else{
+				arr[a] = R[rr];
+				rr++;
+			}
+		}else if(ll < Ln){
+			arr[a] = L[ll];
+			ll++;
+		}else if(rr < Rn){
+			arr[a] = R[rr];
+			rr++;
 		}
-		k++;
-	}
-	while(i < n1){
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-	while(j < n2){
-		arr[k] = R[j];
-		j++;
-		k++;
 	}
 }
