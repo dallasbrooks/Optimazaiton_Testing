08735cbbd575e41816253b92b345ce3082244b2e
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-08-24 15:53:28 -0700

    change stooge to slow alg
    just for fun

--- Resources/Sort/Slow_Sort.c
+++ Resources/Sort/Slow_Sort.c
@@ -3,27 +3,25 @@
 void printArray(int arr[], int n);
 void swap(int *a, int *b);
 
-void stoogeSort(int arr[], int l, int h, int n){
-	if(l >= h){
+void slowSort(int arr[], int l, int r){
+	if(l >= r){
 		return;
 	}
-	if(arr[l] > arr[h]){
-		swap(&arr[l], &arr[h]);
+	int m = (l+r) / 2;
+	slowSort(arr, l, m);
+	slowSort(arr, m+1, r);
+	if(arr[m] > arr[r]){
+		swap(&arr[m], &arr[r]);
 	}
-	if(h-l+1 > 2){
-		printArray(arr, n);
-		int t = (h-l+1) / 3;
-		stoogeSort(arr, l, h-t, n);
-		stoogeSort(arr, l+t, h, n);
-		stoogeSort(arr, l, h-t, n);
-	}
+	slowSort(arr, l, r-1);
 }
 
 int main(){
 	int arr[] = {21, 26, 13, 5, 29, 6, 12, 9, 20, 19, 14, 17, 15, 22, 18, 24, 27, 4, 16, 30, 2, 7, 25, 11, 10};
 	int arr_length = sizeof(arr)/sizeof(arr[0]);
 	printArray(arr, arr_length);
-	stoogeSort(arr, 0, arr_length-1, arr_length);
+	slowSort(arr, 0, arr_length-1);
+	printArray(arr, arr_length);
 	return 0;
 }
 
