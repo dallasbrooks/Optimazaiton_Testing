69d42c0dd50cc556f76316a3e4a1bdfb78b764ba
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-08-24 13:34:13 -0700

    simplify logic

--- Resources/Search/Binary_Search.c
+++ Resources/Search/Binary_Search.c
@@ -3,17 +3,14 @@
 void printArray(int arr[], int n);
 
 int binarySearch(int arr[], int l, int r, int x){
-    if(l > r){
-        return -1;
-    }
-    int m = (l + r) / 2;
-    if(arr[m] == x){
-        return m;
-    }else if(arr[m] > x){
-        return binarySearch(arr, l, m-1, x);
-    }else{
-        return binarySearch(arr, m+1, r, x);
-    }
+	if(l > r){
+		return -1;
+	}
+	int m = (l + r) / 2;
+	if(arr[m] == x){
+		return m;
+	}
+	return arr[m] > x ? binarySearch(arr, l, m-1, x) : binarySearch(arr, m+1, r, x);
 }
 
 int main(){
@@ -28,7 +25,6 @@ int main(){
 	}else{
 		printf("Did not find %d\n", x);
 	}
-	
 	return 0;
 }
 
