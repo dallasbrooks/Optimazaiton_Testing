2b797e16c80a34cbfc17bef44100e62406e2285c
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-08-27 12:17:37 -0700

    remove recursion
    dont want to flood memory

--- Resources/Sort/Bogo_Sort.c
+++ Resources/Sort/Bogo_Sort.c
@@ -10,10 +10,11 @@ void swap(int *a, int *b);
 int isSorted(int arr[], int k, int n);
 
 void bogoSort(int arr[], int n){
-	for(int a = 0; a < n/2; a++){
-		swap(&arr[a], &arr[rand()%n]);
-	}
-	return isSorted(arr, 0, n) ? NULL : bogoSort(arr, n);
+  while(!isSorted(arr, 0, n)){
+    for(int a = 0; a < n/2; a++){
+      swap(&arr[a], &arr[rand()%n]);
+    }
+  }
 }
 
 int main(){
